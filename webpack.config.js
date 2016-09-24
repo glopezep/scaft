4fb26133e508077f9b871b6c1308c29b9258382b
glopezep <guillermolopez2529@gmail.com>
2016-09-24 12:19:21 -0400

    Agregado Webpack y soporte para ES2015, ES2016, ES2017 y Sass

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var extractCSS = new ExtractTextPlugin('public/[name].css')
+
+module.exports = {
+  entry: {
+    app: ['babel-polyfill', './src/index.js']
+  },
+  output: {
+    path: './public',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      { test: /\.scss$/i, loader: extractCSS.extract(['css','sass']) },
+      { test: /\.jsx?$/, loader: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    extractCSS
+  ]
+}
