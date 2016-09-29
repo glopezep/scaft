0193bdd66e775a6d89cd6d26d8f9540f2027b448
glopezep <guillermolopez2529@gmail.com>
2016-09-29 14:55:28 -0400

    Re maquetado el header y la seccion Products

--- server/index.js
+++ server/index.js
@@ -1,14 +1,33 @@
 import express from 'express'
 import { createServer } from 'http'
+import socketio from 'socket.io'
 
-const port = process.env.PORT || 3000
+const port = 3000
 const app = express()
 const server = createServer(app)
+const io = socketio(server)
+let mensajes = []
 
 app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
   res.send(200)
 })
 
-server.listen(port, () => console.log(`Server listening on port ${port}`) )
+app.get('/hola', (req, res) => {
+  res.send(200)
+})
+
+io.on('connection', socket => {
+  console.log(`a user connected ${socket}`)
+  io.emit('fetchMensajes', mensajes)
+  socket.on('disconnect', () => console.log('a user disconnect'))
+  socket.on('conectado', (msg) => {
+    mensajes.push(msg)
+    io.emit('conectado', msg)
+  })
+})
+
+
+server.listen(port, () => console.log(`Server listening on port ${port}`))
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './index.scss'
+import socketio from 'socket.io-client'
+
+const socket = socketio()
+
+window.socket = socket
+
+socket.on('conectado', (msg) => {
+  console.log(msg)
+})
+
+socket.on('fetchMensajes', mensajes => console.log(mensajes))
